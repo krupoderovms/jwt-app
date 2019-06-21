62a76224aa3f811e2ffb64d1a99a362d223dac87
santa <mishka.crash@gmail.com>
2019-06-21 18:51:02 +0500

    Added classes to create and work with jwt tokens

---
+++ src/main/java/com/krupoderov/spring/jwtapp/security/JwtUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.krupoderov.spring.jwtapp.security;
+
+import com.krupoderov.spring.jwtapp.model.User;
+import com.krupoderov.spring.jwtapp.security.jwt.JwtUser;
+import com.krupoderov.spring.jwtapp.security.jwt.JwtUserFactory;
+import com.krupoderov.spring.jwtapp.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("User with username: " + username + " not found");
+        }
+
+        JwtUser jwtUser = JwtUserFactory.create(user);
+        log.info("IN loadUserByUsername - user with username: {} successfully loaded", username);
+
+        return jwtUser;
+    }
+}
